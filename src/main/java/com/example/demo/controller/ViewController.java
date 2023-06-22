176c03ea85896dd7c2cc3cdc3e3e796656974c5e
DongJu-Na <ehdeld123@naver.com>
2023-06-22 15:58:42 +0900

    update

--- src/main/java/com/example/demo/controller/ViewController.java
+++ src/main/java/com/example/demo/controller/ViewController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.net.InetAddress;
 
 @Controller
 public class ViewController {
@@ -16,9 +18,25 @@ public class ViewController {
 		return "demo";
 	}
 	
-	/***************************************************
-	 * Admin Page Start
-	 ***************************************************/
+	@RequestMapping("/myInfo")
+	public String myInfo() {
+		try {
+		  	System.out.println("PC 정보");
+		  	System.out.println();
 
+		  	InetAddress ip = InetAddress.getLocalHost();
+
+		  	System.out.println("Name + IP: " + ip.toString());
+		  	System.out.println("Name:" + ip.getHostName());
+		  	System.out.println("IP address: " + ip.getHostAddress());
+		  	System.out.println("Full name: " + ip.getCanonicalHostName());
+		}
+
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		return "myInfo";
+	}
+	
 	
 }
